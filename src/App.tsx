0b3fead925af66f5a7c6c83197feeb483bba5462
Noah <hanlun.song@gmail.com>
2021-05-11 19:46:06 -0400

    finished the scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,47 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { Suspense } from "react"
+import "./App.css"
+import { Canvas, useLoader } from "@react-three/fiber"
+import { Html, OrbitControls, useProgress } from "@react-three/drei"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
-function App() {
-  const [count, setCount] = useState(0)
+function Scene() {
+  const gltf = useLoader(GLTFLoader, "./model/scene.gltf")
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <primitive
+      object={gltf.scene}
+      scale={[0.005, 0.005, 0.005]}
+      position={[0, -2.5, 0]}
+    />
+  )
+}
+
+function Loader() {
+  const { progress } = useProgress()
+  return (
+    <Html center>
+      <h1>{progress}%</h1>
+    </Html>
+  )
+}
+
+function App() {
+  return (
+    <div className="canvas-container">
+      <Canvas>
+        <Suspense fallback={<Loader />}>
+          <Scene />
+        </Suspense>
+        <OrbitControls autoRotate />
+      </Canvas>
+      <p className="copyright-text">
+        © This work is based on "Alter Saber - Fate/Stay Night: Heaven's Feel"
+        (https://sketchfab.com/3d-models/alter-saber-fatestay-night-heavens-feel-c5223abd18d346a7833b732ae064058c)
+        by Allan Mitsuse (https://sketchfab.com/allanmit) licensed under
+        SKETCHFAB Editorial (https://sketchfab.com/licenses)
+      </p>
     </div>
   )
 }
 
-export default App
+export { App }
